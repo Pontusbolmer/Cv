8424a52872ac2747c048093f39c9707d1d03a5bd
Pontusbolmer <ponteeee@live.se>
2017-12-15 09:57:12 +0000

    Lagt till css, js

---
+++ myscripts.js
@@ -0,0 +1,43 @@
+
+$( document ).ready(function() {
+    
+
+    $(".box1").hide()
+    
+    
+        $("#cool").click (function () {
+         $(".box1").show()
+            
+        });
+
+
+        $("#away").click(function () {
+            $(".box1").hide();
+        
+        });
+
+
+       $(".bajs").hide();
+
+       $("#bajsknapp").click(function () {
+            $(".bajs").show()
+
+       });
+
+
+       $("#släng").click(function () {
+            $(".bajs").hide();
+
+       });
+
+    
+    
+    
+     
+
+
+
+
+
+
+});
